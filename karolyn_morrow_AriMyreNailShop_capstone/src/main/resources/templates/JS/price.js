54eb0b2bde09b3ff5c006565ca760607b099a270
Karolyn Morrow <karolyn.morrow@gmail.com>
2023-06-01 14:32:30 -0400

    Working on JS file for price and extraSvcs html

--- karolyn_morrow_AriMyreNailShop_capstone/src/main/resources/templates/JS/price.js
+++ karolyn_morrow_AriMyreNailShop_capstone/src/main/resources/templates/JS/price.js
@@ -1,24 +1,32 @@
-
+//let x = document.getElementById("addOns").value
 //make a global variable that takes price and is hidden when === 0; add prices to variable onclick based on value: $amount
 
 //Define global variable
 let totalPrice = 0;
+
 //Get html element where price will be shown
 const priceDisplay = document.getElementById("total");
+
 //Get html elements where the buttons are located
 const buttons = document.querySelectorAll("addOns");
+
 //Add an onlick eventlistener to each button
 buttons.forEach(button => {
     button.addEventListener("click", ()=>{
+        //Obtain chosen service from price.html
+
+
         //check if button is pressed
         const isPressed = button.getAttribute("aria-pressed") === "true";
-        //get the value of the button
+        //get the value of the button in extraSvcs.html
         const amountInput = buttons.value;
 
+        if(isPressed){
         totalPrice += amountInput
+        }
 
         if(totalPrice === 0){
-            priceDisplay.style.display = "none";
+            priceDisplay.style.display = "block";
         } else {
             priceDisplay.style.display = "block";
             priceDisplay.innerHTML = `Total Price: ${totalPrice}`;
