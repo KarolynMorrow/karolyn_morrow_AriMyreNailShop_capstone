5b83d9ecb33bca541588b75d2ef6cac3b002f57f
Karolyn Morrow <karolyn.morrow@gmail.com>
2023-07-03 17:15:50 -0400

    Beginning tests

---
+++ src/test/java/com/perscholas/AriMyreNailShop/premium/PremiumAccountServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.perscholas.AriMyreNailShop.premium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class PremiumAccountServiceImplTest {
+
+
+
+}
