79d221729593f02419cb7eed0c47d918236e6d7f
Karolyn Morrow <karolyn.morrow@gmail.com>
2023-07-03 17:08:17 -0400

    No longer need phoneNum validation

--- src/main/resources/static/JS/signUp.js
+++ src/main/resources/static/JS/signUp.js
@@ -1,10 +1,2 @@
- const submitBtn = document.getElementById("submitBtn")
 
-
- 
-submitBtn.addEventListener('click', validate);
-
-const phoneNumIsValid = phoneNumber => {
-    return /^[0-9]{3}[\- ]?[0-9]{3}[\- ]?[0-9]{4}$/.test(phoneNumber);
-}
 
