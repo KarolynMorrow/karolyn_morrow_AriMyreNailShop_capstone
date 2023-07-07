aa40913ef07af0f50f9a13417d40bf63252775a8
Karolyn Morrow <karolyn.morrow@gmail.com>
2023-07-07 10:34:36 -0400

    Working on default mapping for log in

--- src/main/java/com/perscholas/AriMyreNailShop/security/LoginController.java
+++ src/main/java/com/perscholas/AriMyreNailShop/security/LoginController.java
@@ -2,6 +2,7 @@ package com.perscholas.AriMyreNailShop.security;
 
 import com.perscholas.AriMyreNailShop.premium.PremiumAccount;
 import com.perscholas.AriMyreNailShop.premium.PremiumAccountRepository;
+import com.perscholas.AriMyreNailShop.premium.PremiumAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.security.auth.login.AccountNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.nio.file.attribute.UserPrincipal;
 
 @Controller
 public class LoginController {
@@ -23,13 +26,22 @@ public class LoginController {
     @Autowired
     private PremiumAccountRepository premiumAccountRepository;
 
+    private PremiumAccount premiumAccount;
+    private PremiumAccountService service;
+
 
     @GetMapping("/login")
     public String showLogin() {
         return "html/login";
     }
 
-    //default map and use user principal to get premiumAccount.id and route to premiumAccount/{id}
+    //default mapping and use user principal to get premiumAccount.id and route to premiumAccount/{id}
+    @GetMapping("/userProfile")
+    public String userProfilePage(Authentication authentication) throws AccountNotFoundException {
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        PremiumAccount premiumAccountId = service.getPremiumAccountById(premiumAccount.getId());
+        return "redirect:/premiumAccount/" + premiumAccountId;
+    }
 
     @GetMapping("/logout")
     public String logout(HttpServletRequest request) {
