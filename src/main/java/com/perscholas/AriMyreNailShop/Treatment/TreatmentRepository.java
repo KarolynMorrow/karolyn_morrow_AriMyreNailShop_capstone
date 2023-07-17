bfe1803935a29f351088d87c2b405cf2575d45ed
Karolyn Morrow <karolyn.morrow@gmail.com>
2023-07-16 23:43:20 -0400

    Added Repository annotation

--- src/main/java/com/perscholas/AriMyreNailShop/Treatment/TreatmentRepository.java
+++ src/main/java/com/perscholas/AriMyreNailShop/Treatment/TreatmentRepository.java
@@ -1,9 +1,10 @@
 package com.perscholas.AriMyreNailShop.Treatment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface TreatmentRepository extends JpaRepository<Treatment, Integer> {
     Optional<Treatment> findByServiceName(String serviceName);
     Optional<Treatment> findByServiceId(int serviceId);
